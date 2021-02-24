2a1c5ff0716a8bb8a070a19fa48d616675cb6ced
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-02-23 19:51:06 -0700

    fixed App.js and navbar routes.  Moved login and lougout to components catagory

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import login from "./pages/Login";
 import Addfido from "./pages/Addfido";
 import Bio from "./pages/Bio";
 import Like from "./pages/Like";
-import Logout from "./pages/Logout";
+import Homepage from "./pages/Homepage";
 import Updatefido from "./pages/Updatefido";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -16,11 +16,10 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={Homepage} />
           <Route exact path="/addFido" component={Addfido} />
           <Route exact path="/like" component={Like} />
-          <Route exact path="/logout" component={Logout} />
           <Route exact path="/updatefido" component={Updatefido} />
           <Route exact path="/bio" component={Bio} />
         </Wrapper>
---
+++ client/src/components/Logout/Logout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Logout() {
+  return (
+<div>
+  <h1>Woof!</h1>  
+</div>
+  )}
+
+export default Logout;
