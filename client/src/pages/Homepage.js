dc38e534131e8119040523ca1d5b5cd9c9e9ffef
Delmy Melendez <delmymm93@gmail.com>
2021-03-03 18:49:30 -0700

    carousel added to homepage.

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -8,43 +8,44 @@ function Homepage() {
   return (
     <div>
       <Navbar></Navbar>
-      <section id="colorlib-hero" className="js-fullheight" data-section="home">
-        <div className="flexslider js-fullheight">
-          <ul className="slides">
-            <li style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/dogs-playing.png'})` }}>
-              <div className="overlay" />
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                    <div className="slider-text-inner js-fullheight">
-                      <div className="desc">
-                        <h1>Hello Fidos!</h1>
-                        <h2></h2>
-                        <p></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/dogs-playing.png'})` }}>
-              <div className="overlay" />
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                    <div className="slider-text-inner">
-                      <div className="desc">
-                        <h1></h1>
-                        <h2></h2>
-                        <p><a className="btn btn-primary btn-learn"></a></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
+      <section>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./public/img/dog-playing.png"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         <Footer></Footer>
       </section>
     </div>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./components/Login"
 
 function Login() {
     return (
