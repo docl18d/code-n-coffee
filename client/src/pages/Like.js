14a71218fcd015f8d7d2c3f557c5e57a3374547b
Cailin Young <cailinfayyoung@gmail.com>
2021-03-03 19:33:47 -0700

    updating component pages

--- client/src/pages/Like.js
+++ client/src/pages/Like.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
- 
-class Item extends Component {
+
+class Like extends Component {
   state = {
     listitems: ['List Item 1','List Item 2', 'List Item 3']
   };
- 
+
   render() {
     return (
       <React.Fragment>
- 
       </React.Fragment>
     );
   }
 }
+
+export default Like;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Register extends Componen {
+    Render(){
+        Return (
+            <div>
+                <h1>Woof!</h1>  
+            </div>
+        )
+    }
+}
+
+export default Register;
