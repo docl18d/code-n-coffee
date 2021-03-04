a8e7aa91506496130a1f9ac3ea9fa4028ea5dea8
Delmy Melendez <delmymm93@gmail.com>
2021-03-03 21:18:06 -0700

    additional slides added to homepage.

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import Carousel from 'react-bootstrap/Carousel'
 
 
 function Homepage() {
@@ -13,7 +13,7 @@ function Homepage() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="/img/dogs-playing.png"
+              src="/img/dogs-playing1.png"
               alt="First slide"
             />
             <Carousel.Caption>
@@ -24,7 +24,7 @@ function Homepage() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
+              src="/img/dogs-playing2.png"
               alt="Second slide"
             />
 
@@ -36,7 +36,7 @@ function Homepage() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
+              src="/img/dogs-playing3.png"
               alt="Third slide"
             />
 
@@ -45,9 +45,31 @@ function Homepage() {
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
             </Carousel.Caption>
           </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/dogs-playing4.png"
+              alt="Fourth slide"
+            />
+            <Carousel.Caption>
+              <h3>Fourth slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/dogs-playing5.png"
+              alt="Fifth slide"
+            />
+            <Carousel.Caption>
+              <h3>Fifth slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
         </Carousel>
         <div>
-        <Footer /> </div>
+          <Footer /> </div>
       </section>
     </div>
   )
