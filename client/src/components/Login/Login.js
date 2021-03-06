c61d5734373fed12979ac7668ba31aa70b273a00
Delmy Melendez <delmymm93@gmail.com>
2021-03-06 12:04:41 -0700

    browse class added to browse.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./style.css";
+import "./login.css";
 
 
 export default class Login extends Component {
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -64,6 +64,7 @@ function Homepage() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
+
       </section>
     </div>
 
