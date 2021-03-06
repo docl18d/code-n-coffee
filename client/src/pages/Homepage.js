764d9ae14b86eb015186d87bff87df00454b38b4
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-06 10:48:49 -0700

    added list page

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,8 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Carousel from 'react-bootstrap/Carousel'
-
 
 function Homepage() {
   return (
@@ -67,13 +64,9 @@ function Homepage() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-
         <div className='login-btn'>
           <button href="/login.js" type="submit">Login</button>
         </div>
-        <div>
-          <Footer /> </div>
-
       </section>
     </div>
 
