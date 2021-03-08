1e054c11eba867ee944c19ae5cbba67b30ea5518
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-08 00:18:45 -0700

    again

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ useFindAndModify: false,
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => res.sendFile(path.resolve(___dirname, "client", "build", "index.html")));
+  // app.get("*", (req, res) => res.sendFile(path.resolve(___dirname, "client", "build", "index.html")));
 }
 
 // Start the API server
