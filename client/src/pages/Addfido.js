e18ecbb0c3d24bb312980fe4125a7a1ec96172eb
Cailin Young <cailinfayyoung@gmail.com>
2021-03-07 18:02:24 -0700

    pushing

--- client/src/pages/Addfido.js
+++ client/src/pages/Addfido.js
@@ -5,62 +5,62 @@ import ContentCard from "../components/ContentCard";
 
 //my personal suggestion would be to change to class component, set up state like you did
 //in the registration component, and move these inputs down to the render/return
-const Addfido = ({ props }) => {
-  let input;
-  return (
-      <form onSubmit={
-        e => {
-          e.preventDefault();
+// const Addfido = ({ props }) => {
+//   let input;
+//   return (
+//       <form onSubmit={
+//         e => {
+//           e.preventDefault();
 
-          if (!input.value.trim()) {
-            return
-          }
+//           if (!input.value.trim()) {
+//             return
+//           }
 
-          props(Addfido(input.value));
-          input.value = '';
-        }
-      }>
-      <ul>
-        {/* <li>
-          <input type="text" ref={el => (input = el)} />
-          <input type="text" id="" value="" />
-        </li> */}
-        <br />
-        <li>
-          <label for="Breed">Breed:</label>
-          <input type="text" id="" value="" />
-        </li>
-        <li>
-          <label for="Sex">Sex</label>
-          <input type="text" id="Age" value="" />
-        </li>
-        <li>
-          <label for="Age">Last name:</label>
-          <input type="text" id="" value="" />
-        </li>
-        <li>
-          <label for="City">City:</label>
-          <input type="text" id="" value="" />
-        </li>
-        <li>
-          <label for="Park">Park:</label>
-          <input type="text" id="" value="" />
-        </li>
-        <li>
-          <label for="Owners_Preferred_Drink">Owner’s Preffered Drink:</label>
-          <input type="text" id="" value="" />
-        </li>
-        <li>
-          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" type="submit">
-            <i class="material-icons">Add Fido</i>
-          </button>
-        </li>
-          </ul>
-        </form>
+//           props(Addfido(input.value));
+//           input.value = '';
+//         }
+//       }>
+//       <ul>
+//         {/* <li>
+//           <input type="text" ref={el => (input = el)} />
+//           <input type="text" id="" value="" />
+//         </li> */}
+//         <br />
+//         <li>
+//           <label for="Breed">Breed:</label>
+//           <input type="text" id="" value="" />
+//         </li>
+//         <li>
+//           <label for="Sex">Sex</label>
+//           <input type="text" id="Age" value="" />
+//         </li>
+//         <li>
+//           <label for="Age">Last name:</label>
+//           <input type="text" id="" value="" />
+//         </li>
+//         <li>
+//           <label for="City">City:</label>
+//           <input type="text" id="" value="" />
+//         </li>
+//         <li>
+//           <label for="Park">Park:</label>
+//           <input type="text" id="" value="" />
+//         </li>
+//         <li>
+//           <label for="Owners_Preferred_Drink">Owner’s Preffered Drink:</label>
+//           <input type="text" id="" value="" />
+//         </li>
+//         <li>
+//           <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" type="submit">
+//             <i class="material-icons">Add Fido</i>
+//           </button>
+//         </li>
+//           </ul>
+//         </form>
   
-  );
-}
+//   );
+// }
 
 
 
-export default Addfido;
+// export default Addfido;
