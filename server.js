877d49048e5186a3caf2448d4870537f976c7136
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-08 13:20:06 -0700

    fixing heroku white screen issue not reading html page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const path = require('path');
+
+const routes = require('./routes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('client/build'));
 
 // Add routes, both API and view
 app.use(routes);
@@ -29,7 +31,7 @@ useFindAndModify: false,
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("*", (req, res) => res.sendFile(path.resolve(___dirname, "client", "build", "index.html")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(___dirname, "client", "build", "index.html")));
 }
 
 // Start the API server
