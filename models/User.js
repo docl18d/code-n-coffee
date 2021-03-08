b81bf6428725ea734626316f21a43269a186dd57
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-08 00:44:34 -0700

    again

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true, minlength:8},
-})
+});
 
 const User = mongoose.model("User", userSchema);
 
