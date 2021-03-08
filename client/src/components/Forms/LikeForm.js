e17bfdb9ae8be3e8a5dcacead9f8111308c37dd7
Delmy Melendez <delmymm93@gmail.com>
2021-03-07 18:44:18 -0700

    starting styling of add fido page.

--- client/src/components/Forms/LikeForm.js
+++ client/src/components/Forms/LikeForm.js
@@ -3,6 +3,7 @@
 import axios from 'axios';
 import React from 'react';
 
+import "./LikeForm.css";
 
 
 export default class LikeForm extends React.Component {
@@ -28,79 +29,91 @@ export default class LikeForm extends React.Component {
     onSubmit = e => {
         e.preventDefault();
 
-            var Fido_name = this.state.Fido_name
-            var Breed = this.state.Breed
-            var Sex = this.state.Sex
-            var Age = this.state.Age
-            var City = this.state.City
-            var Park = this.state.Park
-            var Owners_Preferred_Drink = this.state.Owners_Preferred_Drink
-        
-        axios.post("/api/fidos", { Fido_name, Breed, Sex, Age, City, Park, Owners_Preferred_Drink }) 
-        .then((response)=>{
-        // SWAP OUT THIS STRING
+        var Fido_name = this.state.Fido_name
+        var Breed = this.state.Breed
+        var Sex = this.state.Sex
+        var Age = this.state.Age
+        var City = this.state.City
+        var Park = this.state.Park
+        var Owners_Preferred_Drink = this.state.Owners_Preferred_Drink
 
-            this.props.history.push('/list')
+        axios.post("/api/fidos", { Fido_name, Breed, Sex, Age, City, Park, Owners_Preferred_Drink })
+            .then((response) => {
+                // SWAP OUT THIS STRING
 
-        })
+                this.props.history.push('/list')
+
+            })
     };
 
     render() {
         console.log(this.state)
         return (
-            <form>
-                <input
-                    name="Fido_Name"
-                    placeholder='Fido Name'
-                    value={this.state.Fido_name}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="Breed"
-                    placeholder='Breed'
-                    value={this.state.Breed}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="Sex"
-                    placeholder='Sex'
-                    value={this.state.Sex}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="Age"
-                    placeholder='Age'
-                    value={this.state.Age}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="City"
-                    placeholder='City'
-                    value={this.state.City}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="Park"
-                    placeholder='Park'
-                    value={this.state.Park}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="Owners_Preferred_Drink"
-                    placeholder='Owners_Preferred_Drink'
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <button onClick={e => this.onSubmit(e)}>Submit</button>
+            <div class="card" style={{ width: "25rem" }}>
+                <form className="form-card">
+                    <h3>Add Fido</h3>
+                    <div className="form-group">
+                        <input
+                            name="Fido_Name"
+                            placeholder='Fido Name'
+                            value={this.state.Fido_name}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            name="Breed"
+                            placeholder='Breed'
+                            value={this.state.Breed}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            name="Sex"
+                            placeholder='Sex'
+                            value={this.state.Sex}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
 
-            </form >
-        );
+                    <div className="form-group">
+                        <input
+                            name="Age"
+                            placeholder='Age'
+                            value={this.state.Age}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            name="City"
+                            placeholder='City'
+                            value={this.state.City}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            name="Park"
+                            placeholder='Park'
+                            value={this.state.Park}
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            name="Owners_Preferred_Drink"
+                            placeholder='Owners Preferred Drink'
+                            onChange={e => this.change(e)}
+                        />
+                    </div>
+                    <button onClick={e => this.onSubmit(e)}>Submit</button>
+                </form >
+            </div>
+        )
     }
 }
-
